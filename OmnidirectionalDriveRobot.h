8876854ed5e68933290914534b4a8dac12fd8c59
williamwright5191 <williamwright5191@gmail.com>
2020-11-13 12:37:28 -0500

    declared variables and functions

--- OmnidirectionalDriveRobot.h
+++ OmnidirectionalDriveRobot.h
@@ -1,4 +1,26 @@
 #include <iostream>
-
+#include "MobileRobot.h"
+#include "Wheel.h"
 
 using namespace std;
+
+class OmnidirectionalDriveRobot : MobileRobot{
+        Wheel frontLeftWheel;
+        Wheel frontRightWheel;
+        Wheel backLeftWheel;
+        Wheel backRightWheel;
+        double lrDist;
+        double fbDistl;
+        double wheelRadius;
+    public:
+        OmnidirectionalDriveRobot(double r, double lrDist, double fbDist );
+        double getVel();
+        double getRotVel();
+        double getVx();
+        double getVy();
+        void print();
+        void setFrontRightWheelVelocity(int velocity);
+        void setFrontLeftWheelVelocity(int velocity);
+        void setBackRightWheelVelocity(int velocity);
+        void setBackLeftWheelVelocity(int velocity);
+};
