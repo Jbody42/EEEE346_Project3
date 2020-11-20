d540b62486e693a212186d39e6e6b64cb812c8d5
williamwright5191 <williamwright5191@gmail.com>
2020-11-20 12:03:59 -0500

    added wheel

--- Wheel.cpp
+++ Wheel.cpp
@@ -1,5 +1,44 @@
 #include <iostream>
 #include "Wheel.h"
-
 using namespace std;
 
+
+Wheel::Wheel(double in_radius, double in_x, double in_y){
+    this->radius = in_radius;
+    this->posX = in_x;
+    this->posY = in_y;
+}
+
+
+double Wheel::getRadius(){
+    return this->radius;
+}
+
+double Wheel::getX(){
+    return this->posX;
+}
+
+double Wheel::getY(){
+    return this->posY;
+}
+
+void Wheel::setX(double in_x){
+    this->posX = in_x;
+}
+
+void Wheel::setY(double in_y){
+    this->posY = in_y;
+}
+
+double Wheel::getVelocity(){
+    return this->velocity;
+}
+
+void Wheel::setVelocity(double in_velocity){
+    this->velocity = in_velocity;
+}
+
+void Wheel::print(){
+    cout << "Position: \t(" << posX << " m, " << posY << " m) \n";
+    cout << "Velocity:\t" << velocity << " rev/s" << endl;
+}
